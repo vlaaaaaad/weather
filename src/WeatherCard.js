08dfbdb91a92c4223635661212fd8d6a0f00f563
vlad <vladyslav.zapryagaylo@fluvius.co>
2020-03-25 16:08:58 +0200

    [Fix] removed initial state in WeatherCard and moved out initial state to constant out of component in WeatherDetails

--- src/WeatherCard.js
+++ src/WeatherCard.js
@@ -36,50 +36,7 @@ const useStyles = makeStyles((theme) => ({
 
 export default function WeatherCard({ city, remove }) {
   const [cityName, setCityName] = useState(city);
-  const [weatherData, setWeatherData] = useState({
-    coord: {
-      lon: 0,
-      lat: 0,
-    },
-    weather: [
-      {
-        id: 0,
-        main: '',
-        description: '',
-        icon: '',
-      },
-    ],
-    base: '',
-    main: {
-      temp: 0,
-      feels_like: 0,
-      temp_min: 0,
-      temp_max: 0,
-      pressure: 0,
-      humidity: 0,
-    },
-    visibility: 0,
-    wind: {
-      speed: 0,
-      deg: 0,
-    },
-    clouds: {
-      all: 0,
-    },
-    dt: 0,
-    sys: {
-      type: 0,
-      id: 0,
-      message: 0,
-      country: '',
-      sunrise: 0,
-      sunset: 0,
-    },
-    timezone: 0,
-    id: 0,
-    name: '',
-    cod: 0,
-  });
+  const [weatherData, setWeatherData] = useState(null);
   const [updateTime, setUpdateTime] = useState(null);
   const [anchorEl, setAnchorEl] = useState(null);
   const [expanded, setExpanded] = useState(false);
--- src/WeatherDetails.js
+++ src/WeatherDetails.js
@@ -9,50 +9,7 @@ export default function WeatherDetails(props) {
   // const [city, setCity] = useState(sessionStorage.getItem("0"));
   const [city, setCity] = useState(props.location.city);
   const [updateTime, setUpdateTime] = useState(null);
-  const [weatherData, setWeatherData] = useState({
-    coord: {
-      lon: 0,
-      lat: 0,
-    },
-    weather: [
-      {
-        id: 0,
-        main: '',
-        description: '',
-        icon: '',
-      },
-    ],
-    base: '',
-    main: {
-      temp: 0,
-      feels_like: 0,
-      temp_min: 0,
-      temp_max: 0,
-      pressure: 0,
-      humidity: 0,
-    },
-    visibility: 0,
-    wind: {
-      speed: 0,
-      deg: 0,
-    },
-    clouds: {
-      all: 0,
-    },
-    dt: 0,
-    sys: {
-      type: 0,
-      id: 0,
-      message: 0,
-      country: '',
-      sunrise: 0,
-      sunset: 0,
-    },
-    timezone: 0,
-    id: 0,
-    name: '',
-    cod: 0,
-  });
+  const [weatherData, setWeatherData] = useState(emptyWeatherData);
 
   useEffect(() => {
     if (props.location.city) {
@@ -154,3 +111,48 @@ export default function WeatherDetails(props) {
     </div>
   );
 }
+
+var emptyWeatherData = {
+  coord: {
+    lon: 0,
+    lat: 0,
+  },
+  weather: [
+    {
+      id: 0,
+      main: '',
+      description: '',
+      icon: '',
+    },
+  ],
+  base: '',
+  main: {
+    temp: 0,
+    feels_like: 0,
+    temp_min: 0,
+    temp_max: 0,
+    pressure: 0,
+    humidity: 0,
+  },
+  visibility: 0,
+  wind: {
+    speed: 0,
+    deg: 0,
+  },
+  clouds: {
+    all: 0,
+  },
+  dt: 0,
+  sys: {
+    type: 0,
+    id: 0,
+    message: 0,
+    country: '',
+    sunrise: 0,
+    sunset: 0,
+  },
+  timezone: 0,
+  id: 0,
+  name: '',
+  cod: 0,
+};
